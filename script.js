5278b19fc8cbc0afca2d2b46034e74a34456f6b6
𝔻𝕙𝕒𝕣𝕞𝕚𝕜 𝕁𝕒𝕘𝕠𝕕𝕒𝕟𝕒 <mailto:dharmik.aatman@gmail.com>
2024-07-17 16:04:42 +0530

    first extension 1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function getMatchData() {
     try {
-        const response = await fetch("https://api.cricapi.com/v1/currentMatches?apikey=e92fd725-b22f-4662-94c4-240d823fada1");
+        return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=bef4b6e4-0fbb-4e11-9410-6e16e2ca5ab1&offset=0")
         const data = await response.json();
 
         if (data.status !== "success") return;
